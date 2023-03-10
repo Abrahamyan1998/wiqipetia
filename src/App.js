5c812a7cc49c9925f82b642ecce23d5974369b5b
Taron1998 <taron.abrahamyan.2018>
2023-03-10 11:36:16 -0800

    wiqi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Container from './Components/Container/Container';
+import Context from './Context';
+
+function App() {
+  return (
+    <Context >
+      <Container />
+    </Context>
+  );
+}
+
+export default App;
